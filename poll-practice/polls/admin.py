1bc4419413498edc57aade7b63e037aaf27d3892
Vincent Nguyen <vincenttsnguyen@gmail.com>
2018-12-04 00:06:53 -0800

    changed admin views

--- poll-practice/polls/admin.py
+++ poll-practice/polls/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question,Choice
 
-admin.site.register(Question)
+
+## create a model admin class, then pass it as the second argument to admin.site.register() – any time you need to change the admin options for a model.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    
+admin.site.register(Question, QuestionAdmin)
